06fb60d373a81c6b568226955f79191b73acfefd
Kiran Anto <kirananto@gmail.com>
2018-01-19 21:42:06 +0530

    Make it response

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="hello">
+  <div class="hello container">
+    <div class="offset-md-4 col-md-5 col-11 offset-1">
+   <div class="row offset-md-2 offset-2">
     <img src="/static/techkshetra_logo.svg" alt="" class="logo"> 
+    </div>
+  <br>
+  <div class="row col-md-4">
     <h1>TECKSHETRA</h1>
+  </div>
+   <div class="row col-md-10 offset-4">
+    <h2>Coming Soon...</h2>
+  </div>
+    </div>
   </div>
 </template>
 
@@ -21,9 +31,18 @@ export default {
 h1, h2 {
   font-weight: normal;
   font-family:'Samarkan Normal';
-  font-size:5rem;
   color:#D6D0D0;
 }
+
+h1 {
+   font-size:5rem;
+}
+@media (max-width: 991px) {
+  h1 {
+    font-size: 3.5rem;
+  }
+}
+
 img {
   width:12rem;
 
@@ -33,6 +52,7 @@ img {
   position: absolute;
   top: 70%;
   right: 50%;
+  max-width: 100%;
   transform: translate(50%,-50%);
 
 
