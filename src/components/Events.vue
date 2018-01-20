927612b9ce170656f2547a2766d638e66d6e0642
Kiran Anto <kirananto@gmail.com>
2018-01-20 11:35:40 +0530

    Add events

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
     <h1>EVENTS</h1>
+    <div class="container-fluid">
+      <br>
+        <div v-for="(event,index) in events" :key='index' class="col-md-3 offset-md-1 card">
+            <h3>{{event.name}}</h3>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +15,12 @@ export default {
   name: 'Events',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      events: [
+      {
+        name: 'Amazing Race',
+        photoURL: '',
+        eventid: 1234
+      }]
     }
   }
 }
@@ -23,9 +34,14 @@ h1, h2 {
   font-size:5rem;
   color:#D6D0D0;
 }
+.card {
+  background-color: white;
+  border-radius: 1rem;
+  height: 10rem;
+}
 .hello {
   position: absolute;
-  top: 50%;
+  top: 60%;
   right: 50%;
   transform: translate(50%,-50%);
   width: 100%;
