7601c6161f605cd865beb1aaa628890f8fa23e2d
Kiran Anto <kirananto@gmail.com>
2018-01-28 19:38:26 +0530

    balco

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello container">
     <img src="/static/techkshetra_logo.svg" alt="" class="logo img-responsive">
+    <p class="powered">powered by </p>
+    <span class="balco">BALCO PVC PIPES AND CONDUITS</span>
   </div>
 </template>
 
@@ -35,6 +37,19 @@ img {
   }
 }
 
+.powered {
+  color: white;
+  margin-bottom: 0rem !important;
+  margin-top: 1rem;
+}
 
+.balco {
+  color: white;
+  /* display: block; */
+  font-size: 1.5rem;
+  font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-stretch: extra-condensed;
+  font-weight: 200;
+}
 
 </style>
