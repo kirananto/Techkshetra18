f3d79978374774a7d602916231ffd9317193ab87
Franklin Antony <franklinanton.007@gmail.com>
2018-01-28 23:54:19 +0530

    Updated Sponsor Page

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,6 +1,51 @@
 <template>
-  <div class="hello">
-    <h1>SPONSORS</h1>
+  <div class="">
+    <h1 class="hmain">SPONSORS</h1>
+
+  <div class="row">
+  <div class="col"></div>
+  <div class="col">
+    
+    <h2 class="hmain">Title Sponsor</h2>
+   
+     <div class="col-4 "><div class="main"><img src="/static/images/temp/1.jpg"></div></div>
+  </div>
+  <div class="col"></div>
+</div>
+
+<br>
+   <h2 class="hmain">Level 1 Sponsor</h2>
+   <br>
+  <div class="row">
+ 
+  <div class="col sub"><img src="/static/images/temp/1.jpg"></div>
+  <div class="col sub"><div class="main"><img src="/static/images/temp/2.jpg"></div>
+  </div>
+  <div class="col sub"><img src="/static/images/temp/3.jpg"></div>
+</div>
+
+<br>
+   <h2 class="hmain">Level 1 Sponsor</h2>
+   <br>
+  <div class="row">
+ 
+  <div class="col sub"><img src="/static/images/temp/1.jpg"></div>
+  <div class="col sub"><div class="main"><img src="/static/images/temp/2.jpg"></div>
+  </div>
+  <div class="col sub"><img src="/static/images/temp/3.jpg"></div>
+</div><br>
+   <h2 class="hmain">Level 1 Sponsor</h2>
+   <br>
+  <div class="row">
+ 
+  <div class="col sub"><img src="/static/images/temp/1.jpg"></div>
+  <div class="col sub"><div class="main"><img src="/static/images/temp/2.jpg"></div>
+  </div>
+  <div class="col sub"><img src="/static/images/temp/3.jpg"></div>
+</div>
+
+
+
   </div>
 </template>
 
@@ -32,4 +77,32 @@ h1, h2 {
   height: 100%;
 
 }
+.main{
+   display: block;
+    margin: 0 auto;
+}
+
+ .sub{
+
+    padding-bottom: 2.5rem;
+  }
+
+
+@media only screen and (max-width:420px){
+  .main{
+       padding-right: 32rem;
+     } 
+     .mob-title{
+     display: block;
+    margin: 0 auto;
+  }  
+    .sub{
+    padding-left: 1.8rem;
+    padding-bottom: 1.5rem;
+  }
+  .hmain{
+    padding-left: 8rem;
+  }
+}
+  
 </style>
