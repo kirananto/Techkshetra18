6e2889e28242ab85992742ff630ce8afffa8b088
Kiran Anto <kirananto@gmail.com>
2018-01-29 01:39:15 +0530

    fix contactUs

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="hello">
     <h1>CONTACT US</h1>
-<div class="row">
-
-     <div class="col-md-6 mcard card">
-      <div class="google-maps">
-        <iframe width="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" align="Center" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=rajagiri+school+of+engineering+%26+Technology&amp;sll=9.993661,76.359251&amp;sspn=0.00858,0.021973&amp;ie=UTF8&amp;hq=rajagiri+school+of+engineering+%26+Technology&amp;hnear=&amp;ll=9.993661,76.359272&amp;spn=0.020287,0.027466&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <p>Rajagiri School of Engineering & Technology</p>
+          <p>Rajagiri Valley, Kakkanad,</p>
+          <p>Kochi 682 039 Kerala INDIA</p>
+          <p>Tel: +91 484 2660999</p>
+          <p>Fax: +91 484 2426241</p>
+        </div>
+        <div class="col-md-6">
+          <div class="google-maps">
+            <iframe width="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" align="Center" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=rajagiri+school+of+engineering+%26+Technology&amp;sll=9.993661,76.359251&amp;sspn=0.00858,0.021973&amp;ie=UTF8&amp;hq=rajagiri+school+of+engineering+%26+Technology&amp;hnear=&amp;ll=9.993661,76.359272&amp;spn=0.020287,0.027466&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
-<br>
-
- <div class="col-md-6 card">
-   <br>
-   <p>Rajagiri School of Engineering & Technology</p><p>Rajagiri School of Engineering & Technology</p><p>Rajagiri School of Engineering & Technology</p><p>Rajagiri School of Engineering & Technology</p>
-  <p>Rajagiri School of Engineering & Technology</p>
-<p>Rajagiri Valley, Kakkanad,</p>
-<p>Kochi 682 039 Kerala INDIA</p>
-<p>Tel: +91 484 2660999</p>
-<p>Fax: +91 484 2426241</p>
-</div></div>
-  </div></div>
 </template>
 
 <script>
@@ -41,34 +39,20 @@ h1, h2 {
   font-size:5rem;
   color:#D6D0D0;
 }
-.hello {
-  position: absolute;
-  top: 50%;
-  right: 50%;
-  transform: translate(50%,-50%);
-  width: 100%;
-  height: 100%;
 
+p {
+  color: white;
 }
-.card{
-  padding: 1.2rem;
-  background-color: black;
-  color: white; 
-}
-@media only screen and (min-width:421px)
-
-{
-  .mcard{
-    /* padding: 50px; */
-  }
+.container {
+  margin-top:5rem;
 }
 .google-maps {
         position: relative;
-        padding-bottom: 75%; // This is the aspect ratio
+        padding-bottom: 75%; 
         height: 0;
         overflow: hidden;
-    }
-    .google-maps iframe {
+}
+.google-maps iframe {
         position: absolute;
         top: 0;
         left: 0;
