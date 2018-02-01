0226a8a4582542ee732fcb08f89ed304011ab350
Franklin Antony <franklinanton.007@gmail.com>
2018-02-01 20:20:37 +0530

    Added new image

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -4,11 +4,11 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <h3 class="hmain">Diamond Sponsors</h3>
+          <h3 class="hmain headerfont">Diamond Sponsors</h3>
           <img src="/static/images/sponsors/BALCO.png" class="col-md-9 sponmargin">
         </div>
         <div class="col-md-6">
-          <h3 class="hmain">Platinum Sponsors</h3>
+          <h3 class="hmain headerfont">Platinum Sponsors</h3>
           <img src="/static/images/sponsors/vguard.png" class="col-md-6 sponmargin">
         </div>
       </div>
@@ -76,5 +76,12 @@ h3 {
      margin-left: 67px;
   }   
 
+}
+
+.headerfont{
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    line-height: 2.5rem;
+    font-weight: 400;
+    padding-top: 2rem;
 }
 </style>
