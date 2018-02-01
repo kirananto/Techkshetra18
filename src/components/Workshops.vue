dd117c70a864e968a5f51aeeb7dae1fd87bbeb90
Kiran Anto <kirananto@gmail.com>
2018-02-01 14:33:09 +0530

    fix scoped css

--- src/components/Workshops.vue
+++ src/components/Workshops.vue
@@ -131,7 +131,7 @@ export default {
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
+<style lang="scss" scoped>
 $content: 'VIEW DETAILS';
 .container {
   margin-top:3rem;
